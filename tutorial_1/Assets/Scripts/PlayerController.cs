fe54050470ae3b185487cb30c1b65385304ed297
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-10 12:55:06 -1000

    Add foward, left and right movements to vehicle. Movment based on frontal rotation, more natural. Some minor organization of assets

--- tutorial_1/Assets/Scripts/PlayerController.cs
+++ tutorial_1/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 16.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 40.0f; // this will be updated 
+    private float horizontalInput;
+    private float fowardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Move the vehicle left or right
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //foward or back
+        fowardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+
+        // rotates from the front. Movment is based off this rotation, looks more natural
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
