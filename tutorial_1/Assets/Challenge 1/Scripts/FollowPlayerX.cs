8c8a43bd4a9c4e9afca0829d8b32a86e4b0e1332
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-12 13:02:30 -1000

    Finish plane challenge for 1.5

---
+++ tutorial_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(20.68f, 1.02f, 0.13f); //offesets camera to a certain position away from player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
