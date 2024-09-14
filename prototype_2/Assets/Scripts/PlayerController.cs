f0d1c9dec1863cf1f409c2ed65d0855e8ec6ebd8
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-13 14:33:11 -1000

    Prototype 2 began. Add players and animals to scene, add food and scripst to players adding boundries

---
+++ prototype_2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 16.0f;
+    public GameObject projectilePrefab; // Object variable
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            // they are constantly put back into the same position, as if there was an invisible wall
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+    }
+}
