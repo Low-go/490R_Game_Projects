3c322a299d106dca0fdbc17c777e6746b7a0c2d8
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-16 20:58:16 -1000

    Continue code 2 project. Add instantiation and destruction to animal and food objects. Make prefabs

--- prototype_2/Assets/Scripts/PlayerController.cs
+++ prototype_2/Assets/Scripts/PlayerController.cs
@@ -31,6 +31,12 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         
     }
 }
