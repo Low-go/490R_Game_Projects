3b20c375e637ed099ceb82e57c2f09369e2b3234
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-25 14:37:29 -1000

    add animations to character movment upon specific conditions

--- Prototype_3/Assets/Scripts/PlayerController.cs
+++ Prototype_3/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,23 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool gameOver;
+    public bool gameOver = false;
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>(); // why animator and not animation
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); //under these conditions, this animation is called
            
         }
     }
@@ -39,6 +42,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true); // death animation aplication
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
