44b4f82de3c83756480c797f5ff00133b83ee8c1
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-26 23:53:57 -1000

    Add particle effects and sound effects to game objects

--- Prototype_3/Assets/Scripts/PlayerController.cs
+++ Prototype_3/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,18 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>(); // why animator and not animation
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,22 +32,27 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig"); //under these conditions, this animation is called
-           
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        
 
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             Debug.Log("Game Over");
+            dirtParticle.Stop();
             gameOver = true;
+            explosionParticle.Play(); // just plays animation under these conditions
             playerAnim.SetBool("Death_b", true); // death animation aplication
             playerAnim.SetInteger("DeathType_int", 1);
         }
