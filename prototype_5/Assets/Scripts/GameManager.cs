84c94934a0eb3dcad21ee523fdd0121cff2387e3
Low-go <lorranalvesgaldino98@gmail.com>
2024-10-08 23:29:13 -1000

    add ui, point system

--- prototype_5/Assets/Scripts/GameManager.cs
+++ prototype_5/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,9 +9,14 @@ public class GameManager : MonoBehaviour
 
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -26,6 +32,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
+            
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
